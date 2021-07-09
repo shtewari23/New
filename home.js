09d19e2cf75324e1dea34b09fdd39e8ea4c026cd
Shubham <shtewari23@gmail.com>
2021-07-09 21:02:18 +0530

    New JS File added

---
+++ home.js
@@ -0,0 +1,34 @@
+    function i(){
+        var x=document.getElementById('info');
+        var y=document.getElementById('info2');
+        if(y.style.display=='block'){
+            y.style.display='none';
+        }
+        else{
+            y.style.display='none';
+        }
+        if(x.style.display=='none'){
+            x.style.display='block';
+
+
+        }
+        else{
+            x.style.display='none';
+        }
+    }
+    function in2(){
+        var y=document.getElementById('info2');
+        var x=document.getElementById('info');
+        if(x.style.display=='block'){
+            x.style.display='none';
+        }
+        else{
+            x.style.display='none';
+        }
+        if(y.style.display=='none'){
+            y.style.display='block';
+        }
+        else{
+            y.style.display='none';
+        }
+    }
