5b176b8434da3276d1455b1123e557b8382b178e
Shubham <shtewari23@gmail.com>
2021-07-10 12:21:22 +0530

    New js file added

---
+++ ques.js
@@ -0,0 +1,24 @@
+  var a=0;
+          var b=0;
+          var c=0;
+          var d=0;
+        function a(){
+          
+            a++;
+            document.getElementById('p').innerHTML=c;
+        }
+        function b(){
+          
+          b++;
+          document.getElementById('p').innerHTML=c;
+      }
+      function c(){
+          
+          c++;
+          document.getElementById('p').innerHTML=c;
+      }
+      function d(){
+          
+          d++;
+          document.getElementById('p').innerHTML=c;
+      }
